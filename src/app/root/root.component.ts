c16c81a19e06d5722a4471348da6042edb1cd81c
Anderson Scherer da Costa <anderson.scherer.costa@gmail.com>
2018-10-08 02:59:57 -0300

    ADD footer and m validations...

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -9,13 +9,13 @@ export class RootComponent implements OnInit {
   title = 'Star Wars - The Saga';
   support = 'The force wil br with you';
   start = false;
+  modulesOff = true;
 
   constructor() { }
 
   ngOnInit() {
-    setTimeout(() => {
-      this.start = true;
-    }, 3000);
+    setTimeout(() => { this.start = true; }, 3000);
+    setTimeout(() => { this.modulesOff = false; }, 6000);
   }
 
   StartModeOn() {
